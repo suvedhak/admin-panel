4053504ba482da6c19fd46d33896f0ccc2479547
suvedhak <68143390+suvedhak@users.noreply.github.com>
2020-12-15 16:41:27 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var url = 'http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
+var url = 'https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D';
 
 $(document).ready(function() {
+
+    $(window).on("load", function() {
+        $("#overlay").fadeOut(500);
+
+    });
 
 $.get(url, function(data) {
 
@@ -22,12 +27,12 @@ class User {
 
     }
     printUserCards() {
-       return(`<tr id="id-${this.idnum}" class="data-row"> 
-       <td class="column1">${this.idnum}</td> 
-       <td class="column2">${this.fname}</td> 
-       <td class="column3">${this.lname}</td> 
-       <td class="column4">${this.email}</td> 
-       <td class="column5">${this.phone}</td> 
+       return(`<tr id="id-${this.idnum}" class="data-row">
+       <td class="column1">${this.idnum}</td>
+       <td class="column2">${this.fname}</td>
+       <td class="column3">${this.lname}</td>
+       <td class="column4">${this.email}</td>
+       <td class="column5">${this.phone}</td>
        </tr>`);
     }
     printUserDetails() {
@@ -53,71 +58,102 @@ class User {
 }
 
 const userCards = $('#user-cards');
-let htmlDetail = [];
-const generateUser = [];
-let htmlstr = " ";
 const searchText = $("#search-box");
+let generateUser = [];
+let htmlDetail = [];
+let htmlstr = " ";
+let detail = [];
 
-for (let i = 0; i < data.length; i++) {
+// Card Detail Function
 
-    generateUser [i] = new User(data[i]);
+function createDetails(detaildata) {
+
+    detail = [];
+    for(let x = 0; x < detaildata.length; x++) {
+
+        detail [x] = $(`#id-${generateUser[x].idnum}`).click(function() {
+
+            $('#info-wrapper').html(htmlDetail[x]);
+
+            $("#info-content").css("display", "block");
+
+            $(".data-row").removeClass("active");
+            $(`#id-${generateUser[x].idnum}`).addClass("active");
+
+        });
+
+    };
+    return(detail);
+}
+
+
+// Create User Cards Function
+
+
+function createCards(apidata) {
+
+htmlDetail = [];
+htmlstr = " ";
+generateUser = [];
+userCards.html(" ");
+
+
+for (let i = 0; i < apidata.length; i++) {
+
+    generateUser [i] = new User(apidata[i]);
 
     htmlstr += generateUser[i].printUserCards();
 
     htmlDetail[i] = generateUser[i].printUserDetails();
-        
-}
 
+
+
+}
 userCards.html(htmlstr);
-
-for (let x = 0; x < data.length; x++) {
-    
-    $(`#id-${generateUser[x].idnum}`).click(function() {
-        
-        $('#info-wrapper').html(htmlDetail[x]);
-        
-        $("#info-content").css("display", "block");
-
-        $(".data-row").removeClass("active");
-        $(`#id-${generateUser[x].idnum}`).addClass("active");
-    
-    });
 }
 
+
+// Create Cards & Details
+
+createCards(data);
+
+createDetails(data);
+
+
+
+// Search Input Filter
 
 document.getElementById("search-box").addEventListener('input', function(e) {
 
     let inputVal = e.target.value.toLowerCase();
-    // console.log(inputVal);
-    
+    console.log(inputVal);
+
     if(inputVal === "") {
-    
-        for (let a = 0; a < data.length; a++) {
-            $(`#id-${data[a].id}`).css("display","block");
-        }
-    
+
+        createCards(data);
+        createDetails(data);
+
     }
-    
+
     else {
         let filterData = [];
         let fnamestr = [];
         for (let y = 0; y < data.length; y++) {
-            
-            $(`#id-${data[y].id}`).css("display","none");
+
+            // $(`#id-${data[y].id}`).css("display","none");
             fnamestr[y] = data[y].firstName.toLowerCase();
-            
+
             if(fnamestr[y].indexOf(inputVal) > -1) {
-    
-                filterData.push(data [y]);
-                // console.log(data[y].id + " " + data[y].firstName + "" +inputVal);
+
+                filterData.push(data[y]);
+                console.log(data[y].id + " " + data[y].firstName + "" +inputVal);
             }
         }
-     
-        for (let b = 0; b < filterData.length; b++) {
-            $(`#id-${filterData[b].id}`).css("display","block");
-            // console.log(filterData[b].id);
-        }
+        console.log(filterData);
+        createCards(filterData);
+        createDetails(filterData);
     }
+
     });
 
 
